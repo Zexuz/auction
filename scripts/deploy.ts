61460669a04e8f28b74d7776546acd69e91da924
Zexuz <robin.edbom@gmail.com>
2023-06-22 18:27:15 +0100

    Write sc address to .env file for easier frontend dev

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,42 @@
-import { ethers } from "hardhat";
+import {ethers} from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const auction = await ethers.deployContract("Auctioneer", {
+        value: ethers.parseEther("0.001"),
+    });
 
-  const lockedAmount = ethers.parseEther("0.001");
+    const res = await auction.waitForDeployment();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+    writeToEnvFile("LOCK_ADDRESS", auction.address);
 
-  await lock.waitForDeployment();
+    console.log("Deployed to:", auction.address);
+}
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+function writeToEnvFile(lockaddress: string, address:string) {
+    const fs = require("fs");
+    const path = require("path");
+    const envPath = path.resolve(__dirname, "../.env");
+    const envExists = fs.existsSync(envPath);
+
+    if (envExists) {
+        const env = fs.readFileSync(envPath).toString();
+        const envLines = env.split("\n");
+        const newEnvLines = envLines.map((line:string) => {
+            const [key, value] = line.split("=");
+            if (key === lockaddress) {
+                return `${key}=${address}`;
+            }
+            return line;
+        });
+        fs.writeFileSync(envPath, newEnvLines.join("\n"));
+    } else {
+        fs.writeFileSync(envPath, `${lockaddress}=${address}`);
+    }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
