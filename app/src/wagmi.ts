f5816d26128cd1bdca4e9beb22e47297e8549681
Zexuz <robin.edbom@gmail.com>
2023-06-29 18:07:12 +0100

    Add support for sepolia

--- app/src/wagmi.ts
+++ app/src/wagmi.ts
@@ -1,5 +1,5 @@
 import {configureChains, createConfig} from 'wagmi'
-import {goerli, mainnet, localhost} from 'wagmi/chains'
+import {goerli, mainnet, localhost, sepolia} from 'wagmi/chains'
 import {CoinbaseWalletConnector} from 'wagmi/connectors/coinbaseWallet'
 import {InjectedConnector} from 'wagmi/connectors/injected'
 import {MetaMaskConnector} from 'wagmi/connectors/metaMask'
@@ -7,22 +7,44 @@ import {WalletConnectConnector} from 'wagmi/connectors/walletConnect'
 
 import {publicProvider} from 'wagmi/providers/public'
 import {jsonRpcProvider} from 'wagmi/providers/jsonRpc'
+import {infuraProvider} from 'wagmi/providers/infura'
 
 const walletConnectProjectId = 'f7a76a6cd8d06210590571118dbb2448'
 
-const {chains, publicClient, webSocketPublicClient} = configureChains(
-    // [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
-    [localhost],
-    [
-        jsonRpcProvider({
-            rpc: (chain) => ({
-                http: `http://localhost:8545`,
-                webSocket: `ws://localhost:8545`,
-            }),
-        }),
-    ],
-)
 
+const getConfig = () => {
+    if (process.env.NEXT_PUBLIC_NETWORK === 'sepolia') {
+
+        const key = process.env.NEXT_PUBLIC_INFURA_API_KEY
+        if (!key) {
+            throw new Error('Missing NEXT_PUBLIC_INFURA_API_KEY')
+        }
+
+        return configureChains(
+            [sepolia],
+            [
+                infuraProvider({
+                    apiKey: key,
+                })
+            ],
+        )
+    }
+
+    return configureChains(
+        // [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
+        [localhost],
+        [
+            jsonRpcProvider({
+                rpc: (chain) => ({
+                    http: `http://localhost:8545`,
+                    webSocket: `ws://localhost:8545`,
+                }),
+            }),
+        ],
+    );
+}
+
+const {chains, publicClient, webSocketPublicClient} = getConfig()
 export const config = createConfig({
     autoConnect: true,
     connectors: [
